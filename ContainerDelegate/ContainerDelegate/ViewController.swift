a0e61c6a3aacbe6be857cab2beaf3ad73014b61e
DonMag <ACP@ACPs-iMac.local>
2018-05-08 10:25:54 -0400

    First Look

---
+++ ContainerDelegate/ContainerDelegate/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  ContainerDelegate
+//
+//  Created by Don Mag on 5/8/18.
+//  Copyright © 2018 DonMag. All rights reserved.
+//
+
+import UIKit
+
+class MyControlsViewController: UIViewController {
+	
+	var delegate: MyControlsDelegate?
+	
+	@IBAction func redTapped(_ sender: Any) {
+		delegate?.changeBackground(UIColor.red)
+	}
+	
+	@IBAction func greenTapped(_ sender: Any) {
+		delegate?.changeBackground(UIColor.green)
+	}
+	
+}
+
+protocol MyControlsDelegate {
+	func changeBackground(_ toColor:UIColor)
+}
+
+class ViewController: UIViewController, MyControlsDelegate {
+	
+	var myControlsVC: MyControlsViewController?
+	
+	@IBOutlet var theLabel: UILabel!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+	func changeBackground(_ toColor: UIColor) {
+		theLabel.backgroundColor = toColor
+	}
+
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if let vc = segue.destination as? MyControlsViewController {
+			myControlsVC = vc
+			myControlsVC?.delegate = self
+		}
+	}
+	
+}
+
